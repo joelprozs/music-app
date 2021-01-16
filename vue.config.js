dded977f8ce9262104d8299658a98d72721c3fca
joelprozs <jozs0319@gmail.com>
2021-01-15 23:48:36 -0500

    config vue2

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*=========================================================================================
+  File Name: vue.config.js
+  Description: configuration file of vue
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuesax Admin - Clean Vue.js Dashboard Admin Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+
+export const publicPath = '/music-app/';
+  
